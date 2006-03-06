3a364c8955d85ab454f7cc5b4cbe3a5c1fa8632e
Hiram R. Chirino <chirino@apache.org>
2006-03-06 16:03:44 +0000

    Moving code in the openwire-dotnet module over to the activemq-dotnet module

---
+++ src/main/csharp/ActiveMQ/Commands/AbstractCommand.cs
@@ -0,0 +1,142 @@
+/*
+ * Copyright 2006 The Apache Software Foundation or its licensors, as
+ * applicable.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using ActiveMQ.Commands;
+using System;
+
+namespace ActiveMQ.Commands
+{
+	
+	/// <summary>
+	/// Summary description for AbstractCommand.
+	/// </summary>
+	public abstract class AbstractCommand : Command
+    {
+        private short commandId;
+        private bool responseRequired;
+        
+        
+        protected AbstractCommand()
+        {
+        }
+        
+        public virtual byte GetDataStructureType()
+        {
+            return 0;
+        }
+        
+        public virtual bool IsMarshallAware()
+        {
+            return false;
+        }
+        
+        
+        
+        // Properties
+        
+        public short CommandId
+        {
+            get { return commandId; }
+            set { this.commandId = value; }
+        }
+        
+        public bool ResponseRequired
+        {
+            get { return responseRequired; }
+            set { this.responseRequired = value; }
+        }
+        
+        public static String GetDataStructureTypeAsString(int type)
+        {
+            String packetTypeStr = "";
+            switch (type)
+            {
+                case ActiveMQMessage.ID_ActiveMQMessage :
+                    packetTypeStr = "ACTIVEMQ_MESSAGE";
+                    break;
+                case ActiveMQTextMessage.ID_ActiveMQTextMessage :
+                    packetTypeStr = "ACTIVEMQ_TEXT_MESSAGE";
+                    break;
+                case ActiveMQObjectMessage.ID_ActiveMQObjectMessage:
+                    packetTypeStr = "ACTIVEMQ_OBJECT_MESSAGE";
+                    break;
+                case ActiveMQBytesMessage.ID_ActiveMQBytesMessage :
+                    packetTypeStr = "ACTIVEMQ_BYTES_MESSAGE";
+                    break;
+                case ActiveMQStreamMessage.ID_ActiveMQStreamMessage :
+                    packetTypeStr = "ACTIVEMQ_STREAM_MESSAGE";
+                    break;
+                case ActiveMQMapMessage.ID_ActiveMQMapMessage :
+                    packetTypeStr = "ACTIVEMQ_MAP_MESSAGE";
+                    break;
+                case MessageAck.ID_MessageAck :
+                    packetTypeStr = "ACTIVEMQ_MSG_ACK";
+                    break;
+                case Response.ID_Response :
+                    packetTypeStr = "RESPONSE";
+                    break;
+                case ConsumerInfo.ID_ConsumerInfo :
+                    packetTypeStr = "CONSUMER_INFO";
+                    break;
+                case ProducerInfo.ID_ProducerInfo :
+                    packetTypeStr = "PRODUCER_INFO";
+                    break;
+                case TransactionInfo.ID_TransactionInfo :
+                    packetTypeStr = "TRANSACTION_INFO";
+                    break;
+                case BrokerInfo.ID_BrokerInfo :
+                    packetTypeStr = "BROKER_INFO";
+                    break;
+                case ConnectionInfo.ID_ConnectionInfo :
+                    packetTypeStr = "CONNECTION_INFO";
+                    break;
+                case SessionInfo.ID_SessionInfo :
+                    packetTypeStr = "SESSION_INFO";
+                    break;
+                case RemoveSubscriptionInfo.ID_RemoveSubscriptionInfo :
+                    packetTypeStr = "DURABLE_UNSUBSCRIBE";
+                    break;
+                case IntegerResponse.ID_IntegerResponse :
+                    packetTypeStr = "INT_RESPONSE_RECEIPT_INFO";
+                    break;
+                case WireFormatInfo.ID_WireFormatInfo :
+                    packetTypeStr = "WIRE_FORMAT_INFO";
+                    break;
+                case RemoveInfo.ID_RemoveInfo :
+                    packetTypeStr = "REMOVE_INFO";
+                    break;
+                case KeepAliveInfo.ID_KeepAliveInfo :
+                    packetTypeStr = "KEEP_ALIVE";
+                    break;
+            }
+            return packetTypeStr;
+        }
+        
+        // Helper methods
+        public int HashCode(object value)
+        {
+            if (value != null)
+            {
+                return value.GetHashCode();
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
+
---
+++ src/main/csharp/ActiveMQ/OpenWire/OpenWireFormat.cs
@@ -0,0 +1,222 @@
+/*
+ * Copyright 2006 The Apache Software Foundation or its licensors, as
+ * applicable.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using ActiveMQ.Commands;
+using ActiveMQ.OpenWire.V1;
+using System;
+using System.IO;
+
+namespace ActiveMQ.OpenWire
+{
+    /// <summary>
+    /// Represents the wire format
+    /// </summary>
+    public class OpenWireFormat
+    {
+        static private char[] MAGIC = new char[] { 'A', 'c', 't', 'i', 'v', 'e', 'M', 'Q' };
+        
+        private BaseDataStreamMarshaller[] dataMarshallers;
+        private const byte NULL_TYPE = 0;
+        private WireFormatInfo wireFormatInfo = new WireFormatInfo();
+        
+        public OpenWireFormat()
+        {
+            // lets configure the wire format
+            wireFormatInfo.Magic = CreateMagicBytes();
+            wireFormatInfo.Version = 1;
+            wireFormatInfo.StackTraceEnabled = true;
+            wireFormatInfo.TcpNoDelayEnabled = true;
+			wireFormatInfo.PrefixPacketSize = true;
+			wireFormatInfo.TightEncodingEnabled = true;
+            
+            dataMarshallers = new BaseDataStreamMarshaller[256];
+            MarshallerFactory factory = new MarshallerFactory();
+            factory.configure(this);
+        }
+        
+        public WireFormatInfo WireFormatInfo {
+            get {
+                return wireFormatInfo;
+            }
+        }
+        
+        public bool StackTraceEnabled {
+            get {
+                return wireFormatInfo.StackTraceEnabled;
+            }
+        }
+        
+        public void addMarshaller(BaseDataStreamMarshaller marshaller)
+        {
+            byte type = marshaller.GetDataStructureType();
+            dataMarshallers[type & 0xFF] = marshaller;
+        }
+        
+        public void Marshal(Object o, BinaryWriter ds)
+        {
+            int size = 1;
+            if (o != null)
+            {
+                DataStructure c = (DataStructure) o;
+                byte type = c.GetDataStructureType();
+                BaseDataStreamMarshaller dsm = dataMarshallers[type & 0xFF];
+                if (dsm == null)
+                    throw new IOException("Unknown data type: " + type);
+                
+                BooleanStream bs = new BooleanStream();
+                size += dsm.TightMarshal1(this, c, bs);
+                size += bs.MarshalledSize();
+                
+                ds.Write(size);
+                ds.Write(type);
+                bs.Marshal(ds);
+                dsm.TightMarshal2(this, c, ds, bs);
+            }
+            else
+            {
+                ds.Write(size);
+                ds.Write(NULL_TYPE);
+            }
+        }
+        
+        public Object Unmarshal(BinaryReader dis)
+        {
+            // lets ignore the size of the packet
+            dis.ReadInt32();
+            
+            // first byte is the type of the packet
+            byte dataType = dis.ReadByte();
+            if (dataType != NULL_TYPE)
+            {
+                BaseDataStreamMarshaller dsm = dataMarshallers[dataType & 0xFF];
+                if (dsm == null)
+                    throw new IOException("Unknown data type: " + dataType);
+                //Console.WriteLine("Parsing type: " + dataType + " with: " + dsm);
+                Object data = dsm.CreateObject();
+                BooleanStream bs = new BooleanStream();
+                bs.Unmarshal(dis);
+                dsm.TightUnmarshal(this, data, dis, bs);
+                return data;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        
+        public int TightMarshalNestedObject1(DataStructure o, BooleanStream bs)
+        {
+            bs.WriteBoolean(o != null);
+            if (o == null)
+                return 0;
+            
+            if (o.IsMarshallAware())
+            {
+                MarshallAware ma = (MarshallAware) o;
+                byte[] sequence = ma.GetMarshalledForm(this);
+                bs.WriteBoolean(sequence != null);
+                if (sequence != null)
+                {
+                    return 1 + sequence.Length;
+                }
+            }
+            
+            byte type = o.GetDataStructureType();
+            if (type == 0) {
+                throw new IOException("No valid data structure type for: " + o + " of type: " + o.GetType());
+            }
+            BaseDataStreamMarshaller dsm = (BaseDataStreamMarshaller) dataMarshallers[type & 0xFF];
+            if (dsm == null)
+                throw new IOException("Unknown data type: " + type);
+            //Console.WriteLine("Marshalling type: " + type + " with structure: " + o);
+            return 1 + dsm.TightMarshal1(this, o, bs);
+        }
+        
+        public void TightMarshalNestedObject2(DataStructure o, BinaryWriter ds, BooleanStream bs)
+        {
+            if (!bs.ReadBoolean())
+                return ;
+            
+            byte type = o.GetDataStructureType();
+            ds.Write(type);
+            
+            if (o.IsMarshallAware() && bs.ReadBoolean())
+            {
+                MarshallAware ma = (MarshallAware) o;
+                byte[] sequence = ma.GetMarshalledForm(this);
+                ds.Write(sequence, 0, sequence.Length);
+            }
+            else
+            {
+                
+                BaseDataStreamMarshaller dsm = (BaseDataStreamMarshaller) dataMarshallers[type & 0xFF];
+                if (dsm == null)
+                    throw new IOException("Unknown data type: " + type);
+                dsm.TightMarshal2(this, o, ds, bs);
+            }
+        }
+        
+        public DataStructure TightUnmarshalNestedObject(BinaryReader dis, BooleanStream bs)
+        {
+            if (bs.ReadBoolean())
+            {
+                
+                byte dataType = dis.ReadByte();
+                BaseDataStreamMarshaller dsm = (BaseDataStreamMarshaller) dataMarshallers[dataType & 0xFF];
+                if (dsm == null)
+                    throw new IOException("Unknown data type: " + dataType);
+                DataStructure data = dsm.CreateObject();
+                
+                if (data.IsMarshallAware() && bs.ReadBoolean())
+                {
+                    dis.ReadInt32();
+                    dis.ReadByte();
+                    
+                    BooleanStream bs2 = new BooleanStream();
+                    bs2.Unmarshal(dis);
+                    dsm.TightUnmarshal(this, data, dis, bs2);
+                    
+                    // TODO: extract the sequence from the dis and associate it.
+                    //                MarshallAware ma = (MarshallAware)data
+                    //                ma.setCachedMarshalledForm(this, sequence);
+                }
+                else
+                {
+                    dsm.TightUnmarshal(this, data, dis, bs);
+                }
+                
+                return data;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        
+        /// <summary>
+        /// Method CreateMagicBytes
+        /// </summary>
+        private byte[] CreateMagicBytes()
+        {
+            byte[] answer = new byte[MAGIC.Length];
+            for (int i = 0; i < answer.Length; i++)
+            {
+                answer[i] = (byte) MAGIC[i];
+            }
+            return answer;
+        }
+    }
+}
