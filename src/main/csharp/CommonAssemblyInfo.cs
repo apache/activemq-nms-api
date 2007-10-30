2b99b0d37da38b29daf31b37b07789c443403626
Hiram R. Chirino <chirino@apache.org>
2007-10-30 00:42:51 +0000

    Updated build

--- src/main/csharp/CommonAssemblyInfo.cs
+++ src/main/csharp/CommonAssemblyInfo.cs
@@ -15,14 +15,14 @@ using System.Runtime.InteropServices;
 [assembly: ComVisibleAttribute(false)]
 [assembly: CLSCompliantAttribute(true)]
 [assembly: AssemblyTitleAttribute("Apache NMS Class Library")]
-[assembly: AssemblyDescriptionAttribute("NMS (.Net Messaging Library): An abstract interface to Message Oriented Middlewar" +
-    "e (MOM) providers")]
+[assembly: AssemblyDescriptionAttribute("Apache NMS (.Net Messaging Library): An abstract interface to Message Oriented Mi" +
+    "ddleware (MOM) providers")]
 [assembly: AssemblyConfigurationAttribute("SNAPSHOT")]
-[assembly: AssemblyCompanyAttribute("http://activemq.apache.org/")]
-[assembly: AssemblyProductAttribute("Apache ActiveMQ")]
+[assembly: AssemblyCompanyAttribute("http://activemq.apache.org/nms")]
+[assembly: AssemblyProductAttribute("Apache NMS Class Library")]
 [assembly: AssemblyCopyrightAttribute("Copyright (C) 2005-2007 Apache Software Foundation")]
 [assembly: AssemblyTrademarkAttribute("")]
 [assembly: AssemblyCultureAttribute("")]
-[assembly: AssemblyVersionAttribute("4.0")]
-[assembly: AssemblyInformationalVersionAttribute("4.0")]
+[assembly: AssemblyVersionAttribute("1.0")]
+[assembly: AssemblyInformationalVersionAttribute("1.0")]
 
