ccfcd06c32ce62cdfd7803eead22eb510836baaf
Jim Gomes <jgomes@apache.org>
2008-07-22 18:41:11 +0000

    Sign the assembly with strong name.  This matches the NAnt build scripts.

--- src/main/csharp/IMessage.cs
+++ src/main/csharp/IMessage.cs
@@ -19,49 +19,48 @@ using System;
 namespace Apache.NMS
 {
 	/// <summary>
-	/// Represents a message either to be sent to a message broker or received from a message broker
+	/// Represents a message either to be sent to a message broker or received from a message broker.
 	/// </summary>
 	public interface IMessage
 	{
 		/// <summary>
-		/// If using client acknowledgement mode on the session then this method will acknowledge that the
+		/// If using client acknowledgement mode on the session, then this method will acknowledge that the
 		/// message has been processed correctly.
 		/// </summary>
 		void Acknowledge();
 		
 		/// <summary>
-		/// Provides access to the message properties (headers)
+		/// Provides access to the message properties (headers).
 		/// </summary>
 		IPrimitiveMap Properties { get; }
 		
 		/// <summary>
-		/// The correlation ID used to correlate messages from conversations or long running business processes
+		/// The correlation ID used to correlate messages from conversations or long running business processes.
 		/// </summary>
 		string NMSCorrelationID { get; set; }
 		
 		/// <summary>
-		/// The destination of the message
+		/// The destination of the message.
 		/// </summary>
 		IDestination NMSDestination { get; }
 		
 		/// <summary>
-		/// The amount of time that this message is valid for.  null If this
-		/// message does not expire.
+		/// The amount of time for which this message is valid.  Zero if this message does not expire.
 		/// </summary>
 		TimeSpan NMSTimeToLive { get; set; }
 		
 		/// <summary>
-		/// The message ID which is set by the provider
+		/// The message ID which is set by the provider.
 		/// </summary>
 		string NMSMessageId { get; }
 		
 		/// <summary>
-		/// Whether or not this message is persistent
+		/// Whether or not this message is persistent.
 		/// </summary>
 		bool NMSPersistent { get; set; }
 		
 		/// <summary>
-		/// The Priority on this message
+		/// The Priority of this message.
 		/// </summary>
 		byte NMSPriority { get; set; }
 		
@@ -82,7 +81,7 @@ namespace Apache.NMS
 		DateTime NMSTimestamp { get; }
 		
 		/// <summary>
-		/// The type name of this message
+		/// The type name of this message.
 		/// </summary>
 		string NMSType { get; set; }
 	}
