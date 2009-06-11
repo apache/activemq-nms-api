a952f959ce75cbd99fed99244f465007069dfd88
Jim Gomes <jgomes@apache.org>
2009-06-11 22:31:58 +0000

    Fix package version numbers.

--- package.ps1
+++ package.ps1
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 $pkgname = "Apache.NMS"
-$pkgver = "1.1.0"
+$pkgver = "1.2.0"
 $configurations = "release", "debug"
 $frameworks = "mono-2.0", "net-1.1", "net-2.0", "net-3.5", "netcf-2.0"
 
