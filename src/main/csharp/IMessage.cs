951f412819333f50fa26f86042a373cf96ad8b1c
Jim Gomes <jgomes@apache.org>
2009-10-02 22:58:05 +0000

    Implemented new BytesMessage API for MSMQ and EMS providers.
    Fixes [AMQNET-162]. (See https://issues.apache.org/activemq/browse/AMQNET-162)

--- src/main/csharp/IMessage.cs
+++ src/main/csharp/IMessage.cs
@@ -28,74 +28,74 @@ namespace Apache.NMS
 		/// message has been processed correctly.
 		/// </summary>
 		void Acknowledge();
-        
-        /// <summary>
-        /// Clears out the message body. Clearing a message's body does not clear its header 
-        /// values or property entries.
-        /// 
-        /// If this message body was read-only, calling this method leaves the message body in 
-        /// the same state as an empty body in a newly created message.
-        /// </summary>
-        void ClearBody();
-        
-        /// <summary>
-        /// Clears a message's properties.
-        /// 
-        /// The message's header fields and body are not cleared.
-        /// </summary>
-        void ClearProperties();
-		
+
+		/// <summary>
+		/// Clears out the message body. Clearing a message's body does not clear its header
+		/// values or property entries.
+		///
+		/// If this message body was read-only, calling this method leaves the message body in
+		/// the same state as an empty body in a newly created message.
+		/// </summary>
+		void ClearBody();
+
+		/// <summary>
+		/// Clears a message's properties.
+		///
+		/// The message's header fields and body are not cleared.
+		/// </summary>
+		void ClearProperties();
+
 		/// <summary>
 		/// Provides access to the message properties (headers).
 		/// </summary>
 		IPrimitiveMap Properties { get; }
-		
+
 		/// <summary>
 		/// The correlation ID used to correlate messages from conversations or long running business processes.
 		/// </summary>
 		string NMSCorrelationID { get; set; }
-		
+
 		/// <summary>
 		/// The destination of the message.  This property is set by the IMessageProducer.
 		/// </summary>
 		IDestination NMSDestination { get; }
-		
+
 		/// <summary>
 		/// The amount of time for which this message is valid.  Zero if this message does not expire.
 		/// </summary>
 		TimeSpan NMSTimeToLive { get; set; }
-		
+
 		/// <summary>
 		/// The message ID which is set by the provider.
 		/// </summary>
 		string NMSMessageId { get; }
-		
+
 		/// <summary>
 		/// Whether or not this message is persistent.
 		/// </summary>
 		MsgDeliveryMode NMSDeliveryMode { get; set; }
-	
+
 		/// <summary>
 		/// The Priority of this message.
 		/// </summary>
 		MsgPriority NMSPriority { get; set; }
-		
+
 		/// <summary>
 		/// Returns true if this message has been redelivered to this or another consumer before being acknowledged successfully.
 		/// </summary>
 		bool NMSRedelivered { get; }
-		
+
 		/// <summary>
 		/// The destination that the consumer of this message should send replies to
 		/// </summary>
 		IDestination NMSReplyTo { get; set; }
-		
+
 		/// <summary>
-		/// The timestamp of when the message was pubished in UTC time.  If the publisher disables setting 
+		/// The timestamp of when the message was pubished in UTC time.  If the publisher disables setting
 		/// the timestamp on the message, the time will be set to the start of the UNIX epoc (1970-01-01 00:00:00).
 		/// </summary>
 		DateTime NMSTimestamp { get; }
-		
+
 		/// <summary>
 		/// The type name of this message.
 		/// </summary>
