e120a93b559b79d77feaf911b0ff4cbedb111848
Timothy A. Bish <tabish@apache.org>
2009-10-29 17:59:21 +0000

    Add additional documentation to the Acknowledgment modes.

--- src/main/csharp/IConnection.cs
+++ src/main/csharp/IConnection.cs
@@ -18,93 +18,96 @@ using System;
 
 namespace Apache.NMS
 {
-	/// <summary>
-	/// The mode used to acknowledge messages after they are consumed
-	/// </summary>
-	public enum AcknowledgementMode
-	{
-		/// <summary>
-		/// With this acknowledgment mode, the session will not
-		/// acknowledge receipt of a message since the broker assumes
-		/// successful receipt of a message after the onMessage handler
-		/// has returned without error.
-		/// </summary>
-		AutoAcknowledge,
+    /// <summary>
+    /// The mode used to acknowledge messages after they are consumed
+    /// </summary>
+    public enum AcknowledgementMode
+    {
+        /// <summary>
+        /// With this acknowledgment mode, the session will not
+        /// acknowledge receipt of a message since the broker assumes
+        /// successful receipt of a message after the onMessage handler
+        /// has returned without error.
+        /// </summary>
+        AutoAcknowledge,
 
-		/// <summary>
-		/// With this acknowledgment mode, the session automatically
-		/// acknowledges a client's receipt of a message either when
-		/// the session has successfully returned from a call to receive
-		/// or when the message listener the session has called to
-		/// process the message successfully returns.  Acknowlegements
-		/// may be delayed in this mode to increase performance at
-		/// the cost of the message being redelivered this client fails.
-		/// </summary>
-		DupsOkAcknowledge,
+        /// <summary>
+        /// With this acknowledgment mode, the session automatically
+        /// acknowledges a client's receipt of a message either when
+        /// the session has successfully returned from a call to receive
+        /// or when the message listener the session has called to
+        /// process the message successfully returns.  Acknowlegements
+        /// may be delayed in this mode to increase performance at
+        /// the cost of the message being redelivered this client fails.
+        /// </summary>
+        DupsOkAcknowledge,
 
-		/// <summary>
-		/// With this acknowledgment mode, the client acknowledges a
-		/// consumed message by calling the message's acknowledge method.
+        /// <summary>
+        /// With this acknowledgment mode, the client acknowledges a
+        /// consumed message by calling the message's acknowledge method.
         /// This acknowledgement acknowledges the given message and all
-        /// unacknowedged messages that have preceeded it.
-		/// </summary>
-		ClientAcknowledge,        
+        /// unacknowedged messages that have preceeded it for the session
+        /// in which the message was delivered.
+        /// </summary>
+        ClientAcknowledge,
 
-		/// <summary>
-		/// Messages will be consumed when the transaction commits.
-		/// </summary>
-		Transactional,
+        /// <summary>
+        /// Messages will be consumed when the transaction commits.
+        /// </summary>
+        Transactional,
 
         /// <summary>
         /// With this acknowledgment mode, the client acknowledges a
         /// consumed message by calling the message's acknowledge method.
         /// This acknowledgement mode allows the client to acknowledge a
-        /// single message.
+        /// single message.  This mode is not required to be supported by
+        /// all NMS providers, however the provider should throw an appropriate
+        /// exception to indicate that the mode is unsupported.
         /// </summary>
         IndividualAcknowledge
-	}
+    }
 
-	/// <summary>
-	/// A delegate that can receive transport level exceptions.
-	/// </summary>
-	public delegate void ExceptionListener(Exception exception);
+    /// <summary>
+    /// A delegate that can receive transport level exceptions.
+    /// </summary>
+    public delegate void ExceptionListener(Exception exception);
 
     /// <summary>
     /// A delegate that is used by Fault tolerant NMS Implementation to notify their
-    /// clients that the Connection is not currently active to due some error.  
+    /// clients that the Connection is not currently active to due some error.
     /// </summary>
     public delegate void ConnectionInterruptedListener();
 
     /// <summary>
     /// A delegate that is used by Fault tolerant NMS Implementation to notify their
-    /// clients that the Connection that was interrupted has now been restored.  
+    /// clients that the Connection that was interrupted has now been restored.
     /// </summary>
     public delegate void ConnectionResumedListener();
-    
-	/// <summary>
-	/// Represents a connection with a message broker
-	/// </summary>
-	public interface IConnection : IDisposable, IStartable, IStoppable
-	{
-		/// <summary>
-		/// Creates a new session to work on this connection
-		/// </summary>
-		ISession CreateSession();
 
-		/// <summary>
-		/// Creates a new session to work on this connection
-		/// </summary>
-		ISession CreateSession(AcknowledgementMode acknowledgementMode);
+    /// <summary>
+    /// Represents a connection with a message broker
+    /// </summary>
+    public interface IConnection : IDisposable, IStartable, IStoppable
+    {
+        /// <summary>
+        /// Creates a new session to work on this connection
+        /// </summary>
+        ISession CreateSession();
 
-		/// <summary>
-		/// Closes the connection.
-		/// </summary>
-		void Close();
+        /// <summary>
+        /// Creates a new session to work on this connection
+        /// </summary>
+        ISession CreateSession(AcknowledgementMode acknowledgementMode);
 
-		/// <summary>
-		/// An asynchronous listener which can be notified if an error occurs
-		/// </summary>
-		event ExceptionListener ExceptionListener;
+        /// <summary>
+        /// Closes the connection.
+        /// </summary>
+        void Close();
+
+        /// <summary>
+        /// An asynchronous listener which can be notified if an error occurs
+        /// </summary>
+        event ExceptionListener ExceptionListener;
 
         /// <summary>
         /// An asynchronous listener that is notified when a Fault tolerant connection
@@ -117,30 +120,30 @@ namespace Apache.NMS
         /// has been resumed.
         /// </summary>
         event ConnectionResumedListener ConnectionResumedListener;
-        
-		#region Attributes
 
-		/// <summary>
-		/// The default timeout for network requests.
-		/// </summary>
-		TimeSpan RequestTimeout { get; set; }
+        #region Attributes
 
-		/// <summary>
-		/// The default acknowledgement mode
-		/// </summary>
-		AcknowledgementMode AcknowledgementMode { get; set; }
+        /// <summary>
+        /// The default timeout for network requests.
+        /// </summary>
+        TimeSpan RequestTimeout { get; set; }
 
-		/// <summary>
-		/// Sets the unique clienet ID for this connection before Start() or returns the
-		/// unique client ID after the connection has started
-		/// </summary>
-		string ClientId { get; set; }
+        /// <summary>
+        /// The default acknowledgement mode
+        /// </summary>
+        AcknowledgementMode AcknowledgementMode { get; set; }
 
-		/// <summary>
-		/// Gets the Meta Data for the NMS Connection instance.
-		/// </summary>
-		IConnectionMetaData MetaData{ get; }
+        /// <summary>
+        /// Sets the unique clienet ID for this connection before Start() or returns the
+        /// unique client ID after the connection has started
+        /// </summary>
+        string ClientId { get; set; }
 
-		#endregion
-	}
+        /// <summary>
+        /// Gets the Meta Data for the NMS Connection instance.
+        /// </summary>
+        IConnectionMetaData MetaData{ get; }
+
+        #endregion
+    }
 }
