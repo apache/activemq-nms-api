a4f3da611b4a32a4207f065137b49a554ce6ace4
Timothy A. Bish <tabish@apache.org>
2010-03-17 13:48:12 +0000

    http://issues.apache.org/activemq/browse/AMQNET-244
    Make these inherit from IQueue and ITopic and add the Delete method to the API.

--- src/main/csharp/ITemporaryTopic.cs
+++ src/main/csharp/ITemporaryTopic.cs
@@ -20,8 +20,17 @@ namespace Apache.NMS
 	/// Represents a temporary topic which exists for the duration 
 	/// of the IConnection which created it.
 	/// </summary>
-	public interface ITemporaryTopic : IDestination
+	public interface ITemporaryTopic : ITopic
 	{
+		/// <summary>
+		/// Deletes this Temporary Destination, If there are existing receivers 
+		/// still using it, a NMSException will be thrown.
+		/// </summary>
+        /// <exception cref="Apache.NMS.NMSException">
+        /// If NMS Provider fails to Delete the Temp Destination or the client does
+        /// not support this operation.
+        /// </exception>
+		void Delete();		
 	}
 }
 
