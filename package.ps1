f2417cf441ce8d9b0e72d455787e49c7dff8315b
Timothy A. Bish <tabish@apache.org>
2010-05-10 14:38:10 +0000

    Update maintenance branch version to 1.3.1-SNAPSHOT

--- package.ps1
+++ package.ps1
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 $pkgname = "Apache.NMS"
-$pkgver = "1.3-RC1"
+$pkgver = "1.3.1-SNAPSHOT"
 $configurations = "release", "debug"
 $frameworks = "mono-2.0", "net-2.0", "net-3.5", "netcf-2.0", "netcf-3.5"
 
